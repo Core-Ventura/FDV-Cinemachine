aed95bd8d1bb4b42e1ebe57fe39c5fd43283eea2
Coré Ventura <core.ventura@gmail.com>
2019-12-03 20:51:58 +0000

    Add files via upload

---
+++ CameraEnabler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEnabler : MonoBehaviour
+{
+    public GameObject Camera1;
+    public GameObject Camera2;
+    private bool enabled = true;
+    void Start()
+    {
+        Camera1.SetActive(enabled);
+        Camera2.SetActive(!enabled);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C)){
+            enabled = !enabled;
+            Camera1.SetActive(enabled);
+            Camera2.SetActive(!enabled);
+        }
+    }
+}
---
+++ ImpulseGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ImpulseGenerator : MonoBehaviour
+{
+
+    public CinemachineImpulseSource impulseSource;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            impulseSource.GenerateImpulse();
+        }
+    }
+
+}
